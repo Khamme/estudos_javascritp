1f94a9bb8ed248870daed87c7b9895a1045a7f2d
Carlos Eduardo <78832858+Khamme@users.noreply.github.com>
2021-03-17 08:31:43 -0300

    Add files via upload

---
+++ javascritp/aula05/ex008.js
@@ -0,0 +1,13 @@
+/*var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto')*/
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else {
+    console.log('Você é Estrangeiro')
+}
